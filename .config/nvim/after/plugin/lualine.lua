5f6775cdc231ace492614168b3969e2d6db0a81e
owlsly94 <stefan.stanimirovic09@gmail.com>
2024-02-06 20:48:50 +0100

    lua line changes

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,13 +1,16 @@
 require("lualine").setup {
     options = {
       icons_enabled = true,
-      theme = "onedark"
+      theme = "catppuccin",
+      section_separators = { left = "", right = "" },
     },
     selections = {
       lualine_a = {
         {
           "filename",
-          path = 1
+          path = 1,
+          fg = "#1c1d21",
+          bg = "#b4befe",
         }
       }
     }
