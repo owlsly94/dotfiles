3afeb012c3e196d724dcdf0af0e1df085637b282
owlsly94 <stefan.stanimirovic09@gmail.com>
2024-06-09 23:02:38 +0200

    xmonad

--- .config/xmonad/scripts/cpu-temp.sh
+++ .config/xmonad/scripts/cpu-temp.sh
@@ -4,7 +4,7 @@
 SENSOR_OUTPUT=$(sensors k10temp-pci-00c3)
 
 # Use grep to find the line containing 'Tctl' and awk to extract the temperature, then strip non-numeric characters
-TCTL_TEMP=$(echo "$SENSOR_OUTPUT" | grep 'Tctl' | awk '{print $2}' | sed 's/+//;s/°C//')
+TCTL_TEMP=$(echo "$SENSOR_OUTPUT" | grep 'Tctl' | awk '{print $2}' | sed 's/+//;s/°C//' | awk '{printf "%.0f\n", $1}' | tail -n 2)
 
 # Print only the temperature number
 echo " $TCTL_TEMP°C"
