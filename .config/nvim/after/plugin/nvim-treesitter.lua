c770cc6abce468681fdd820c44e93326c4fb6496
owlsly <stefan.stanimirovic09@gmail.com>
2024-07-26 00:22:40 +0200

    nvim treesitter configs

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -4,12 +4,18 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
   highlight = {
     enable = true,
-    disable = function(lang, buf)
+    disable = function(_, buf)
         local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
+        local filename = vim.api.nvim_buf_get_name(buf)
+        local handle = io.popen("stat -c %s " .. filename)
+        if handle then
+          local result = handle:read("*a")
+          handle:close()
+          if result and tonumber(result) > max_filesize then
             return true
+          end
         end
+        return false
     end,
     additional_vim_regex_highlighting = false,
   },
