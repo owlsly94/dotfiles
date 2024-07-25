3367bd48b8ada367d26c92f0927124742d46fe6e
owlsly <stefan.stanimirovic09@gmail.com>
2024-07-25 23:58:36 +0200

    nvim colors - tokyonight

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -1,11 +1,24 @@
-require("catppuccin").setup({
-  flavour = "mocha",
-  transparent_background = true,
+require("tokyonight").setup({
+  style = "night",
+--  style = "storm",
+--  style = "moon",
+  transparent = true,
   styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" }, -- Change the style of comments
+    functions = {}, -- Change the style of comments
   },
 })
 
-vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
+
+--require("catppuccin").setup({
+--  flavour = "mocha",
+--  transparent_background = true,
+--  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--    comments = { "italic" }, -- Change the style of comments
+--  },
+--})
+--
+--vim.o.termguicolors = true
+--vim.o.background = "dark"
+--vim.cmd[[colorscheme catppuccin]]
