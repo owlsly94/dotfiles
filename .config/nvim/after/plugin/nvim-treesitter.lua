d9c062bc3e9e07954884037949817ab19d216a6e
owlsly <stefan.stanimirovic09@gmail.com>
2024-07-26 14:23:04 +0200

    nvim small changes

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -7,27 +7,28 @@ require'nvim-treesitter.configs'.setup {
     disable = function(_, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local filename = vim.api.nvim_buf_get_name(buf)
-        print("Filename: " .. tostring(filename)) -- Debug print
 
+        -- Ensure the filename is valid and not empty
         if not filename or filename == "" then
           return false
         end
 
         local handle = io.popen("stat -c %s " .. filename)
-
         if handle then
           local result = handle:read("*a")
           handle:close()
-          print("File size result: " .. tostring(result)) -- Debug print
 
+          -- Ensure the result is valid and convert to number
           local file_size = tonumber(result)
-          print("File size: " .. tostring(file_size)) -- Debug print
-
-          if file_size and file_size > max_filesize then
+          -- Log only if file_size is nil
+          if not file_size then
+            print("Error: Unable to determine file size for " .. filename)
+          elseif file_size > max_filesize then
             return true
           end
+        else
+          print("Error: Unable to open handle for " .. filename)
         end
-
         return false
     end,
     additional_vim_regex_highlighting = false,
