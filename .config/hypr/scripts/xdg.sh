9562d01648eb0bb70f94f07f66e79df507c655d4
owlsly94 <stefan.stanimirovic09@gmail.com>
2025-05-31 23:14:17 +0200

    xdg portals script

--- .config/hypr/scripts/xdg.sh
+++ .config/hypr/scripts/xdg.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
+
+systemctl --user stop xdg-desktop-portal xdg-desktop-portal-hyprland
+
+killall -q xdg-desktop-portal-hyprland
+killall -q xdg-desktop-portal
+
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+
+sleep 1
+
 /usr/lib/xdg-desktop-portal &
+
